def71a28bfc8c89bb76f096563a1e57554ae541e
dinaameil <dinaameil753@gmail.com>
2025-03-26 11:15:31 +0700

    Searching / Pencarian Menggunakan Binary Search

--- MahasiswaDemo08.java
+++ MahasiswaDemo08.java
@@ -20,18 +20,22 @@ public class MahasiswaDemo08 {
             System.out.println("-------------------------------");
             list.tambah (new Mahasiswa08(nim, nama, kelas, ipk));
         }
-            list.tampil();
-            System.out.println("----------------------------------");
-            System.out.println("Pencarian Data");
-            System.out.println("----------------------------------");
-            System.out.println("Masukkan ipk mahasiswa yang ducari: ");
-            System.out.print("IPK :");
-            double cari = sc.nextDouble();
 
-            System.out.println("mengguanakan sequential searching");
-            double posisi = list.sequentialSearching(cari);
-            int pss = (int) posisi;
-            list.tampilPosisi(cari, pss);
-            list.tampilDataSearch(cari, pss);
+        list.tampil();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("-------------------------------------------------");
+        System.out.println("Masukkan IPK mahasiswa yang dicari: ");
+        System.out.print("IPK : ");
+        double cari = sc.nextDouble();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Menggunakan binary search");
+        System.out.println("-------------------------------------------------");
+        
+        double posisi2 = list.findBinarySearch(cari, 0, jumMhs - 1);
+        int pss2 = (int) posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
+
     }
 }
